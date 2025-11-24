2f4dde1d058397f8e009794d065c1ff1aceb5328
Chris <cbonner.dev@outlook.com>
2025-11-24 18:13:47 +0000

    created watches table

---
+++ database/migrations/2025_11_24_175639_create_watches_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('watches', function (Blueprint $table) {
+            $table->id();
+            $table->integer("brand_id")->unsigned();
+            $table->integer("supplier_id")->unsigned();
+
+            // TODO: NEED TO FIND WATCH CATEGORIES $table->enum();
+
+            $table->decimal("price");
+            $table->text("name");
+            $table->text("description");
+            $table->integer("size");
+            $table->timestamps();
+
+            $table
+                ->foreign("brand_id")
+                ->references("id")
+                ->on("brands")
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table
+                ->foreign("supplier_id")
+                ->references("id")
+                ->on("suppliers")
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('watches');
+    }
+};
