4e83b6c2d70595e6fcdda4e3de791c3b8c62d3bc
Chris <cbonner.dev@outlook.com>
2025-11-24 18:16:44 +0000

    created inventory table

---
+++ database/migrations/2025_11_24_181400_create_inventories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->integer("watch_id")->unsigned();
+            $table->integer("quantity");
+            $table->timestamps();
+
+            $table
+                ->foreign("watch_id")
+                ->references("id")
+                ->on("watches")
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventories');
+    }
+};
