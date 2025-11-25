c3de2fd2a2b61db431891c1a818f141fc7111cb5
Chris <cbonner.dev@outlook.com>
2025-11-25 12:33:44 +0000

    implemented authentication with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get("/signup", function () {
-    return view("signup");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get("/login", function () {
-    return view("login");
-});
-
-Route::get("/about", function () {
-    return view("about");
-});
-
-Route::get("/contact", function () {
-    return view("contact");
-});
-
-Route::get("/product_edits", function () {
-    return view("product_edits");
-});
+require __DIR__.'/auth.php';
