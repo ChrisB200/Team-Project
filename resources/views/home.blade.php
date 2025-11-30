c883339919b2bb425f17f6814af533e380a27b3c
Chris <cbonner.dev@outlook.com>
2025-11-30 12:17:12 +0000

    quick fix for the user pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.user')
 
 @section('page')
-  <p>
-    HOME PAGE
-  </p>
+  <section>
+    <h1>
+      CROWN & DIAL
+    </h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque
+      sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis
+      imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu..
+    </p>
+    <button class="accent-button">Find Out More</button>
+  </section>
 @stop
