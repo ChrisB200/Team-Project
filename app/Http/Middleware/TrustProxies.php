94ef4ca1913d6cd4008226e8fa2385a300b09f56
Chris <cbonner.dev@outlook.com>
2025-12-05 19:01:35 +0000

    added trust proxy middleware

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust *all* proxies (recommended when behind Docker or Nginx reverse proxy)
+     */
+    protected $proxies = '*';
+
+    /**
+     * Use all standard forwarded headers to correctly detect HTTPS.
+     */
+    protected $headers =
+    Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
+}
